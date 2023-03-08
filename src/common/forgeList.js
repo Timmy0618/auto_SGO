f699d1cacd20120b2f772ebbed5e9289b50ed904
Timmy <timmy85618@gmail.com>
2023-03-08 17:31:20 +0800

    更新鍛造表

--- src/common/forgeList.js
+++ src/common/forgeList.js
@@ -1,24 +1,15 @@
 const forgeList = [
-  {
-    name: "短刀",
-    type: "dagger",
-  },
-  {
-    name: "單手劍",
-    type: "sword",
-  },
-  {
-    name: "盾牌",
-    type: "shield",
-  },
-  {
-    name: "細劍",
-    type: "rapier",
-  },
-  {
-    name: "雙手斧",
-    type: "axe",
-  },
+  { name: "短刀", type: "dagger" },
+  { name: "單手劍", type: "sword" },
+  { name: "細劍", type: "rapier" },
+  { name: "單手錘", type: "hammer" },
+  { name: "盾牌", type: "shield" },
+  { name: "雙手斧", type: "axe" },
+  { name: "雙手劍", type: "thsword" },
+  { name: "太刀", type: "katana" },
+  { name: "長槍", type: "spear" },
+  { name: "大衣", type: "coat" },
+  { name: "盔甲", type: "armor" },
 ];
 
 export default forgeList;
